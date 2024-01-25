417da568261bf9df67a267581544cc98de42d58c
YUNDAEWON1 <dwsb0905@naver.com>
2024-01-25 09:53:59 +0900

    다익스트라

--- BaeKjoon/1753 최단경로.py
+++ BaeKjoon/1753 최단경로.py
@@ -1,38 +1,47 @@
 import sys
-input=sys.stdin.readline
+
 from queue import PriorityQueue
+INF=int(1e9)
 
-V, E=map(int, input().split())  #V노드갯수 E엣지갯수
-K=int(input())  # K= 출발노드
-distance=[sys.maxsize]*(V+1)   #거리저장리스트 충분히 큰 값으로
-visited=[False]*(V+1)   #방문여부 저장 리스트
-myList=[[] for _ in range(V+1)]  #엣지 데이터 저장 인접 리스트
-q=PriorityQueue()  #다익스트라를 위한 우선순위 큐
+input=sys.stdin.readline
 
 
-# 인접리스트에 데이터 저장
-for _ in range(E):
-    u,v,w=map(int, input().split())   # 출발지 도착지 가중치
-    myList[u].append((v, w))
+V,E=map(int,input().split())  # V는 노드갯수 E는 엣지갯수
+K=int(input())  # 출발지
 
-q.put((0, K))
-distance[K]=0
+distance=[INF]*(V+1)  #거리정보 저장 리스트
+visited=[False]*(V+1) #방문여부 저장 리스트
+graph=[[] for _ in range(V+1)] # 엣지정보 저장 리스트
+q=PriorityQueue() # 우선순위 큐
 
-while q.qsize()>0:
-    current=q.get()
-    c_v = current[1]
-    if visited[c_v]:
-        continue
-    visited[c_v]=True
-    for temp in myList[c_v]:
-        next = temp[0]
-        value=temp[1]
-        if distance[next] > distance[c_v]+value:
-            distance[next]=distance[c_v]+value
-            q.put((distance[next],next))
+for i in range(E):
+    s, e, w = map(int,input().split())
+    graph[s].append([e,w])
+
+
+def dijkstra(graph, start):
+    q.put((0,start)) # 출발노드 큐에 넣고 시작
+    distance[start]=0 # 거리리스트의 출발 노드 값을 0으로 설정
+
+    while q:
+        current=q.get()   # 우선순위 큐에서 노드 가져오기
+        c_v=current[1]    
+        if visited[c_v]:  # 현재 선택된 노드가 방문한적있으면 continue
+            continue
+        visited[c_v]=True    # 방문체크
+        for temp in graph[c_v]:  # 현재 선택노드의 엣지 정보
+            next_node=temp[0]    # 다음 노드
+            next_weight=temp[1]  # 다음 노드의 가중치
+            if distance[next_node] >distance[c_v]+next_weight:
+                distance[next_node]=distance[c_v]+next_weight
+                q.put((distance[next_node],next_node))
+    return distance
+
+
+dijkstra(graph,K)
 
 for i in range(1,V+1):
-    if visited[i]:
+    if distance[i]!=INF:
         print(distance[i])
     else:
-        print('INF')
+        print("INF")
